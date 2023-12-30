6b79bc744ca4b7e351527bac0c7deecf28506831
Viorel Breban <vbreban69@gmail.com>
2023-12-30 16:35:00 +0100

    final version

--- src/js/dark-theme/dark-theme.js
+++ src/js/dark-theme/dark-theme.js
@@ -1,12 +1,10 @@
+let isDarkMode = false;
 const root = document.documentElement;
-const toggle = document.getElementById('toggle-button');
-const darkMode = localStorage.getItem('dark-mode');
-if (darkMode) {
-  root.classList.add('dark-theme');
+const darkModeSwitch = document.querySelector('.switch__input');
+
+darkModeSwitch.addEventListener('click', toggleColorTheme);
+
+function toggleColorTheme() {
+  isDarkMode = !isDarkMode;
+  root.classList.toggle('dark-theme', isDarkMode);
 }
-toggle.addEventListener('click', () => {
-  root.classList.toggle('dark-theme');
-  if (root.classList.contains('dark-theme')) {
-    localStorage.removeItem('dark-mode');
-  }
-});
